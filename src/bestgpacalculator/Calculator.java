b93c22e674e0ef0ea00d2ff463524c8c7068c968
James Bernard <jamezilla0@gmail.com>
2016-05-07 13:43:11 -0400

    Started methods for Calculator class. - Grade Constructor(int number); - Grade Constructor(String character); - DecryptGrade() -> Will convert letter to value vise versa - getLetterGrade() -> returns letter of grade - getValue() -> returns 'double' value of grade

--- src/bestgpacalculator/Calculator.java
+++ src/bestgpacalculator/Calculator.java
@@ -1,9 +1,43 @@
 package bestgpacalculator;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Steven
  */
 public class Calculator {
     //Algorithm for calculating the GPA
+    private ArrayList<Grade> gradeList = new ArrayList<>();
+    
+    public Calculator(){
+        
+    }
+
+    private static class Grade {
+        private String letter;
+        private double value;
+        
+        public Grade(int number) {
+            
+        }
+        
+        public Grade(String character){
+            
+        }
+        
+        public void decryptGrade(){
+            
+        }
+        
+        public String getLetterGrade(){
+            return letter;
+        }
+        
+        public double getValue(){
+            return value;
+        }
+    }
+    
+    
 }
