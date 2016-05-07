0d94586e0078c6b8157eb5f60c18f7696b12c62d
James Bernard <jamezilla0@gmail.com>
2016-05-07 16:07:53 -0400

    Finished gpa calculations regardless of letter or number grade inputs.

--- src/bestgpacalculator/Calculator.java
+++ src/bestgpacalculator/Calculator.java
@@ -13,4 +13,27 @@ public class Calculator {
     public Calculator(){
         
     }
+    
+    public void addGrade(Grade grade){
+        gradeList.add(grade);
+    }
+    
+    public void getGpa(){
+        int count = gradeList.size();
+        double total = 0;
+        
+        for(Grade grade : gradeList){
+            total += grade.getValue();
+//            System.out.println("Grade Value: " + grade.getValue());
+        }
+        
+//        System.out.println("Total score: " + total);
+        
+        double gpa = total / count;
+        
+        String gpaLetter = Grade.decrypt.decryptNumberGrade(gpa);
+        
+        System.out.println("Your GPA: " + gpa);
+        System.out.println("Your GPA Letter Grade: " + gpaLetter);
+    }
 }
