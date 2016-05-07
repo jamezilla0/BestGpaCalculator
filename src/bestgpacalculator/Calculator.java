bf23582dded2ad1ae5330b76b8dcd33e646d13b9
Stevenalmonte <Stevenalmonte4@outlook.com>
2016-05-07 14:22:00 -0400

    Added the range to the letters grade in the decryptletterGrade() //Steven

--- src/bestgpacalculator/Calculator.java
+++ src/bestgpacalculator/Calculator.java
@@ -17,6 +17,7 @@ public class Calculator {
     private static class Grade {
         private String letter;
         private double value;
+        private double range[];
         
         public Grade(int number) {
             
@@ -27,7 +28,70 @@ public class Calculator {
         }
         
         public void decryptletterGrade(){
-           
+            switch (letter) {
+                case "A+":
+                    range[0] = 97.5;
+                    range[1] = 100;                   
+                    break;
+                    
+                case "A":
+                    range[0] = 92.5;
+                    range[1] = 97.4;
+                    break;
+                    
+                case "A-":
+                    range[0] = 90.0;
+                    range[1] = 92.4;
+                    break;
+                    
+                    case "B+":
+                    range[0] = 87.5;
+                    range[1] = 89.9;
+                    break;
+                    
+                    case "B":
+                    range[0] = 82.5;
+                    range[1] = 87.4;
+                    break;
+                    
+                    case "B-":
+                    range[0] = 80.0;
+                    range[1] = 82.4;
+                    break;
+                    
+                    case "C+":
+                    range[0] = 75.5;
+                    range[1] = 79.9;
+                    break;
+                    
+                    case "C":
+                    range[0] = 72.5;
+                    range[1] = 75.4;
+                    break;
+                    
+                    case "C-":
+                    range[0] = 70.0;
+                    range[1] = 72.4;
+                    break;
+                    
+                    case "D+":
+                    range[0] = 67.5;
+                    range[1] = 69.9;
+                    break;
+                    
+                    case "D":
+                    range[0] = 62.5;
+                    range[1] = 67.4;
+                    break;
+                    
+                    case "D-":
+                    range[0] = 60.0;
+                    range[1] = 62.4;
+                    break;
+                default:
+                    range[0] = 0.0;
+                    range[1] = 59.9;
+            }
         }
         
         public String getLetterGrade(){
