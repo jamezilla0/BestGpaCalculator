6338272cf2b76c6eb143765baaaa3a362b4f3487
James Bernard <jamezilla0@gmail.com>
2016-05-07 15:12:32 -0400

    Tested conversion for number and letter grades using Decryptor class conversion methods.

--- src/bestgpacalculator/BestGpaCalculator.java
+++ src/bestgpacalculator/BestGpaCalculator.java
@@ -5,6 +5,8 @@
  */
 package bestgpacalculator;
 
+import java.util.Scanner;
+
 /**
  * @Class CST1201
  * @Profesor Michael Best
@@ -21,12 +23,21 @@ package bestgpacalculator;
  * @JDBX Read Method
  */
 public class BestGpaCalculator {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         // TODO code application logic here
+        
+        System.out.print("Enter a number grade: ");
+        double userInput = input.nextDouble();
+        String letterGrade = Grade.decrypt.decryptNumberGrade(userInput);
+        System.out.println("Your letter grade is: " + letterGrade);
+        
+        System.out.println("Converting grade to number range");
+        double[] range = Grade.decrypt.decryptletterGrade(letterGrade);
+        System.out.printf("Your grade is between %.1f and %.1f \n", range[0], range[1]);        
     }   
         
     
